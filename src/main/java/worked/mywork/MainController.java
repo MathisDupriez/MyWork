bdccae40dd32c3ae6216091d555019084d41abbe
MathisDupriez <la217548@student.helha.be>
2023-11-25 23:41:11 +0100

    Adding top bar on the window

--- src/main/java/worked/mywork/MainController.java
+++ src/main/java/worked/mywork/MainController.java
@@ -3,12 +3,8 @@ package worked.mywork;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class MainController {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+
 }
